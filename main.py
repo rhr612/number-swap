180cc76b463d197d1e768f9a02da2204ffdfbda4
RHR <rakibhassanrakib612@gmail.com>
2023-01-16 01:07:13 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Hello!!!!!! What is your Name?')
+name=input()
+print('hello '+name +'   Input a')
+a=input()
+print('Nice!!!!!!!   Now input b')
+b=input()
+
+print('You entered :'+'\n A= '+a + '\n B= ' + b)
+
+c=a
+a=b
+b=c
+
+print ('After swaping :\n'+' A= '+a+'\n B= '+b)
